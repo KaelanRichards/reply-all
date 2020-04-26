aae245997cd1649c4d4d80e089309841c9aeacd1
Kaelan Richards <kado@Kaelans-MacBook-Air.local>
2020-04-26 17:43:07 -0400

    added firebase integtration, email and password auth, facebook auth

--- app/main.js
+++ app/main.js
@@ -1,17 +1,32 @@
-import Vue from 'nativescript-vue'
-import App from './components/App'
-import VueDevtools from 'nativescript-vue-devtools'
+import Vue from "nativescript-vue";
+import VueDevtools from "nativescript-vue-devtools";
+// import App from './components/App'
+import LoginPage from "./components/LoginPage";
 
-if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
+if (TNS_ENV !== "production") {
+  Vue.use(VueDevtools);
 }
-import store from './store'
+import store from "./store";
 
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+Vue.config.silent = TNS_ENV === "production";
 
+var firebase = require("nativescript-plugin-firebase");
+firebase
+  .init({
+    // Optionally pass in properties for database, authentication and cloud messaging,
+    // see their respective docs.
+  })
+  .then(
+    function(instance) {
+      console.log("firebase.init done");
+    },
+    function(error) {
+      console.log("firebase.init error: " + error);
+    }
+  );
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
-}).$start()
+  render: (h) => h("frame", [h(LoginPage)]),
+}).$start();
